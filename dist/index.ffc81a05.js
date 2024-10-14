945aaa4753423b01476b528ed77d625de7092870
František Rapčák (CS) <Frankie.Rapcak@gendigital.com>
2024-10-14 14:05:08 +0200

    add dist folder

---
+++ dist/index.ffc81a05.js
@@ -0,0 +1,2 @@
+!function(){let t=document.querySelector(".sticky-bar"),e=document.querySelector(".js-show-sticky-bar");window.addEventListener("scroll",function(){e.getBoundingClientRect().top<=0?t.classList.add("is-sticky"):t.classList.remove("is-sticky")})}();
+//# sourceMappingURL=index.ffc81a05.js.map
